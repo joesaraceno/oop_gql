967ddec66fb5ec6223d988aebd263c491c211967
Joe Saraceno <josephasaraceno@gmail.com>
2019-02-18 01:03:11 -0800

    slightly fix more tests

--- controllers/ShoppingCart.js
+++ controllers/ShoppingCart.js
@@ -8,22 +8,34 @@ module.exports = class ShoppingCart {
   }
 
   addItem(item) {
+    if (typeof item === 'undefined' || item == null) {
+      throw new Error('can\'t add empty Book')
+    }
     this._items.push(item);
   }
 
   removeItem(isbn) {
     let index = this._items.find(item => item.isbn === isbn);
+    if (typeof index == 'undefined' || index < 0) {
+      throw new Error('can\'t find book to remove')
+    }
     this._items.splice(index, 1);
   }
 
   getReceipt() {
     console.log('Purchasing items...')
     let total = this.calculateTotal();
+    if (!this._id || typeof this._id === 'undefined') {
+      throw new Error('can\'t find cart ID');
+    }
     console.log(`Purchased $${total} of books.\nTransaction Id: ${this._id}`)
     this._items = [];
   }
 
   calculateTotal() {
+    if (this.getNumberOfItems() === 0) {
+      return 0;
+    }
     return this._items.reduce((value, item) => {
       return value + item._price;
     }, 0)
--- tests/ShoppingCart.spec.js
+++ tests/ShoppingCart.spec.js
@@ -2,31 +2,19 @@ import test from 'ava'
 import ShoppingCart from '../controllers/ShoppingCart';
 
 test('should throw when doing bad stuff', t => {
-  const cart = new ShoppingCart();
-  const foo = {
-
-  };
-
-  const bar = {
-
-  };
-  
+  const cart = new ShoppingCart(); // the test cart
   t.throws(() => {
-    cart.add(undefined);
+    cart.addItem(undefined); // add an empty book
+  })
+  t.throws(() => {
+    cart.removeItem({isbn: -1});
   })
 })
 
 test('should not throw when not doing bad stuff', t => {
-  const cart = new ShoppingCart();
-  const foo = {
-
-  };
-
-  const bar = {
-
-  };
+  const cart = new ShoppingCart(); // the test cart
+  const book = {}; // placeholder for a book
   t.notThrows(() => {
-    cart.addItem(foo);
-
+    cart.addItem(book); // add the book object
   })
 })
