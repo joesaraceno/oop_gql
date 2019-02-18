ac5b9e4195d118f30911a5962f80be2395ed9876
Joe Saraceno <josephasaraceno@gmail.com>
2019-02-18 00:41:42 -0800

    gql initial

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const parser = require('body-parser')
 const express = require('express')
 const _ = require('lodash')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const gqlPort = 4001;
 
 const app = express()
 
@@ -8,7 +12,33 @@ app.use(parser.json())
 
 app.use(parser.urlencoded({ extended: true }))
 
+// graphql initialization of root schema
+const schema = buildSchema(`
+  "The root of it all"
+  type Query {
+    "Returns a greeting"
+    hello: String
+  }
+`)
 
+const root = {
+  hello: () => {
+    return 'Hello World!';
+  }
+}
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+
+app.listen(gqlPort)
+
+console.log(`Gql API Server Running on ${gqlPort}`);
+
+// restful routes
 app.get('/status', (req, res) => {
   res.send({
     status: 'ok'
--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const server = http.createServer(app)
 const PORT = process.env.NODE_ENV || 5050
 
 server.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}`)
+  console.log(`base http server is listening on port ${PORT}`)
 })
